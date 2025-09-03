cdc1c33a4eaa07f12980b8438e56de7e0471b269
etotty <44785384+etotty@users.noreply.github.com>
2025-09-03 20:26:32 +0000

    Deploying to gh-pages from @ etotty/etotty.github.io@f8f6759d6dfe9e6c5dd30302559e7f285383bae9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -35,21 +35,21 @@ ninja.data = [{
           description: "publications by categories in reversed chronological order",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/publications_factormodels/";
+            window.location.href = "/publications_factormodels";
           },
         },{id: "nav-publications",
           title: "publications",
           description: "publications by categories in reversed chronological order",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/publications_minimumwage/";
+            window.location.href = "/publications_minimumwage";
           },
         },{id: "nav-publications",
           title: "publications",
           description: "publications by categories in reversed chronological order",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/publications_other/";
+            window.location.href = "/publications_other";
           },
         },{id: "nav-cv",
           title: "cv",
