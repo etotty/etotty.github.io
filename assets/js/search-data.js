fb1b8ebab89bf21d960536f60435629af9e0b192
etotty <44785384+etotty@users.noreply.github.com>
2025-09-03 19:44:04 +0000

    Deploying to gh-pages from @ etotty/etotty.github.io@bc36a74a9c472c5c3d8927346035cc359f582dda 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/education";
           },
+        },{id: "nav-publications",
+          title: "publications",
+          description: "publications by categories in reversed chronological order",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/factormodels";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "",
