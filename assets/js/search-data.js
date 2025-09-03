14eea8d79205298b19e8569b370fef41b979753b
etotty <44785384+etotty@users.noreply.github.com>
2025-09-03 20:42:42 +0000

    Deploying to gh-pages from @ etotty/etotty.github.io@861c56efbb3241081a5d5fc23c3ec40d62a5eebb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -21,7 +21,35 @@ ninja.data = [{
           description: "publications by categories in reversed chronological order",
           section: "Navigation",
           handler: () => {
+            window.location.href = "/publications_dataprivacy/";
+          },
+        },{id: "nav-publications",
+          title: "publications",
+          description: "publications by categories in reversed chronological order",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications_education/";
+          },
+        },{id: "nav-publications",
+          title: "publications",
+          description: "publications by categories in reversed chronological order",
+          section: "Navigation",
+          handler: () => {
             window.location.href = "/publications_factormodels/";
+          },
+        },{id: "nav-publications",
+          title: "publications",
+          description: "publications by categories in reversed chronological order",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications_minimumwage/";
+          },
+        },{id: "nav-publications",
+          title: "publications",
+          description: "publications by categories in reversed chronological order",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications_other/";
           },
         },{id: "nav-cv",
           title: "cv",
